43617f7ffd55ce245b938d35206c8645f5d19a9f
Dennis Lee <dennis.li@hacknocraft.com>
2018-01-16 17:03:15 +0800

    initialize as android project

---
+++ app/src/main/java/com/dennis/baseprojectandroid/application/AndroidApplication.java
@@ -0,0 +1,17 @@
+package com.dennis.baseprojectandroid.application;
+
+import android.app.Application;
+
+/**
+ * Created by BY on 2018/1/16.
+ */
+
+public class AndroidApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Do something while initialing application.
+    }
+}
